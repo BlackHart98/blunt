e20256825df35a0dd22defa7340b6255e964843a
Pius Arhanbhunde <pjacks419@gmail.com>
2024-07-22 23:58:28 +0100

    chore(): clear up lexer module

---
+++ src/lexer.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, PartialEq)]
+pub enum Lexeme<'a>{
+    Keyword{token: &'a str, pos: u32, length: u32},
+    SpecialChar{token: &'a str, pos: u32, length: u32},
+    WhiteSpace{token: &'a str, pos: u32, length: u32},
+    Number{token: &'a str, pos: u32, length: u32},
+    BinOp{token: &'a str, pos: u32, length: u32},
+    Comment{token: &'a str, pos: u32, length: u32},
+    ForwardArrow{token: &'a str, pos: u32, length: u32},
+    Yield{token: &'a str, pos: u32, length: u32},
+    UnsupportedToken{token: &'a str, pos: u32, length: u32},
+    Identifier{token: &'a str, pos: u32, length: u32}
+}
+
+
+pub fn scan_string<'a>(input : &'a str) -> Result<Vec<Lexeme>, &'static str>{
+    let mut foo = String::from(input);
+    println!("Scanning... {:?}", foo);
+    return Err("scanner failed");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-#[derive(Debug, PartialEq)]
-enum Lexeme<'a>{
-    Keyword{token: &'a str, pos: u32, length: u32},
-    SpecialChar{token: &'a str, pos: u32, length: u32},
-    WhiteSpace{token: &'a str, pos: u32, length: u32},
-    Number{token: &'a str, pos: u32, length: u32},
-    BinOp{token: &'a str, pos: u32, length: u32},
-    Comment{token: &'a str, pos: u32, length: u32},
-    ForwardArrow{token: &'a str, pos: u32, length: u32},
-    Yield{token: &'a str, pos: u32, length: u32},
-    UnsupportedToken{token: &'a str, pos: u32, length: u32},
-    Identifier{token: &'a str, pos: u32, length: u32}
-}
-
-
-fn scan_string<'a>(input : &'a str) -> Result<Vec<Lexeme>, &'static str>{
-    let mut foo = String::from(input);
-    println!("Scanning... {:?}", foo);
-    return Err("scanner failed");
-}
-
+use avatre::lexer;
 
 fn main() {
-    // let foo = vec![Lexeme::Keyword{token: "fn", pos: 0, length: 2}];
-    println!("Hello, world! {:?}", scan_string("input"));
+    let mut foo = vec![lexer::Lexeme::Keyword{token: "fn", pos: 0, length: 2}];
+    // println!("Hello, world! {:?}", lexer::scan_string("input"));
+    println!("Hello, world! {:?}", foo);
 }
