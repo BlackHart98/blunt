0e36ce014583d82c2151a38b1ad450378bc3a614
Pius Arhanbhunde <pjacks419@gmail.com>
2024-07-23 13:25:05 +0100

    feat(): added reserved words array

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,25 @@
+use std::collections::HashSet;
+
+#[allow(dead_code)]
+
+
+
+const RESERVED_WORDS: [&'static str; 36] 
+    = [
+        // keywords
+        "fn", "if", "@import", "@extend", "let",
+        "const", "return", "visit", "top_down", "bottom_up",
+        "innermost", "fail", "insert", "outermost", "top_down_break",
+        "for", "elif", "else","@external","@sypnosis","typedef",
+        "data",
+
+         // data types
+        "any","num","int","str","real",
+        "list","tuple","rel","lrel","map",
+        "void","set","node","loc",
+    ];
+
+
 #[derive(Debug, PartialEq)]
 pub enum Lexeme{
     Keyword{token: String, pos: usize, length: usize},
@@ -5,7 +27,8 @@ pub enum Lexeme{
     WhiteSpace{token: String, pos: usize, length: usize},
     Number{token: String, pos: usize, length: usize},
     BinOp{token: String, pos: usize, length: usize},
-    Comment{token: String, pos: usize, length: usize},
+    CommentSingleLine{token: String, pos: usize, length: usize},
+    CommentMultiLine{token: String, pos: usize, length: usize},
     ForwardArrow{token: String, pos: usize, length: usize},
     Yield{token: String, pos: usize, length: usize},
     UnsupportedChar{token: String, pos: usize, length: usize},
@@ -14,17 +37,29 @@ pub enum Lexeme{
     StringLiteralDouble{token: String, pos: usize, length: usize},
 }
 
-
 pub fn scan_input<'a>(input : &str) -> Result<Vec<Lexeme>, &'static str>{
     let mut result = Vec::<Lexeme>::new();
+    let keywords = HashSet::from(RESERVED_WORDS);
+
+    // TODO: Implement scanner (stub)
     for (i, c) in input.chars().enumerate(){
         match c {
             ';' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            '+' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            '-' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            '*' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            '^' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            '`' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            '=' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            '/' => result.push(Lexeme::SpecialChar{token: c.to_string(), pos: i, length: 1}),
+            ' ' => result.push(Lexeme::WhiteSpace{token: c.to_string(), pos: i, length: 1}),
+            '\n' => result.push(Lexeme::WhiteSpace{token: c.to_string(), pos: i, length: 1}),
+            '\t' => result.push(Lexeme::WhiteSpace{token: c.to_string(), pos: i, length: 1}),
             _ => result.push(Lexeme::UnsupportedChar{token: c.to_string(), pos: i, length: 1}),
         }
     }
-    let foo = has_unsupported_char(&result);
-    if foo {
+    
+    if !has_unsupported_char(&result) {
       return Err("unsupported character");  
     }   else{
         return Ok(result);
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use avatre::lexer;
 
 fn main() {
-    // let mut foo = vec![lexer::Lexeme::Keyword{token: "fn", pos: 0, length: 2}];
-    println!("Hello, world! {:?}", lexer::scan_input("return 0;"));
-    // println!("Hello, world! {:?}", foo);
+    println!("avatre 0.0.0 \n {:?}", lexer::scan_input("+ ;"));
 }
